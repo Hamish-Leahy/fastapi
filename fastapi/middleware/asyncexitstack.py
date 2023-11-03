4514c22d63b74f89d67412a3d47cf7fc86910ac3
Hamish Leahy <98940688+Hamish-Leahy@users.noreply.github.com>
2023-11-03 13:52:21 +1100

    Cleaned

--- fastapi/middleware/asyncexitstack.py
+++ fastapi/middleware/asyncexitstack.py
@@ -19,7 +19,5 @@ class AsyncExitStackMiddleware:
                 dependency_exception = e
                 raise e
         if dependency_exception:
-            # This exception was possibly handled by the dependency but it should
-            # still bubble up so that the ServerErrorMiddleware can return a 500
-            # or the ExceptionMiddleware can catch and handle any other exceptions
+           
             raise dependency_exception
